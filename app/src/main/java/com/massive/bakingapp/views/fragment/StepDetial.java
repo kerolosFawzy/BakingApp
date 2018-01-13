67c788cae866afa38f78c43dec7f455b6af08b31
gentel_mina2011 <gentel_mina2011@hotmail.com>
2018-01-13 15:32:11 +0200

    partly worked

--- app/src/main/java/com/massive/bakingapp/views/fragment/StepDetial.java
+++ app/src/main/java/com/massive/bakingapp/views/fragment/StepDetial.java
@@ -3,40 +3,26 @@ package com.massive.bakingapp.views.fragment;
 import android.app.Fragment;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
-import android.view.Surface;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import com.google.android.exoplayer2.DefaultLoadControl;
-import com.google.android.exoplayer2.ExoPlaybackException;
-import com.google.android.exoplayer2.ExoPlayer;
 import com.google.android.exoplayer2.ExoPlayerFactory;
-import com.google.android.exoplayer2.Format;
-import com.google.android.exoplayer2.LoadControl;
-import com.google.android.exoplayer2.PlaybackParameters;
 import com.google.android.exoplayer2.SimpleExoPlayer;
-import com.google.android.exoplayer2.Timeline;
-import com.google.android.exoplayer2.decoder.DecoderCounters;
 import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
-import com.google.android.exoplayer2.extractor.ExtractorsFactory;
-import com.google.android.exoplayer2.source.LoopingMediaSource;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
 import com.google.android.exoplayer2.source.MediaSource;
-import com.google.android.exoplayer2.source.TrackGroupArray;
-import com.google.android.exoplayer2.source.hls.HlsMediaSource;
 import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
-import com.google.android.exoplayer2.trackselection.TrackSelection;
-import com.google.android.exoplayer2.trackselection.TrackSelectionArray;
 import com.google.android.exoplayer2.trackselection.TrackSelector;
 import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
 import com.google.android.exoplayer2.upstream.BandwidthMeter;
 import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
-import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
 import com.google.android.exoplayer2.util.Util;
-import com.google.android.exoplayer2.video.VideoRendererEventListener;
 import com.massive.bakingapp.R;
 import com.massive.bakingapp.models.Steps;
 
@@ -44,41 +30,37 @@ import com.massive.bakingapp.models.Steps;
  * Created by minafaw on 1/12/2018.
  */
 
-public class StepDetial extends Fragment implements VideoRendererEventListener {
+public class StepDetial extends Fragment {
 
     Uri videoUri;
     private Steps steps;
     private SimpleExoPlayerView simpleExoPlayerView;
     private SimpleExoPlayer player;
-    private TextView Description;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View RootView = inflater.inflate(R.layout.step_detial_fragment, container, false);
-        Description = new TextView(getActivity());
-        Description = RootView.findViewById(R.id.Description);
+        TextView description = new TextView(getActivity());
+        description = RootView.findViewById(R.id.Description);
         simpleExoPlayerView = new SimpleExoPlayerView(getActivity());
         simpleExoPlayerView = RootView.findViewById(R.id.videoView);
         Bundle bundle = getArguments();
         int id = bundle.getInt("StepId");
         steps = CardFragment.steps.get(id);
-        Description.setText(steps.getDescription());
-        showData();
+        description.setText(steps.getDescription());
+        exoplayerShow();
+        //showData();
         return RootView;
     }
 
 
-
-    private void showData() {
+    private void exoplayerShow() {
+        String userAgent = Util.getUserAgent(getActivity(), "bakingapp");
         BandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
-        TrackSelection.Factory videoTrackSelectionFactory = new AdaptiveTrackSelection.Factory(bandwidthMeter);
-        TrackSelector trackSelector = new DefaultTrackSelector(videoTrackSelectionFactory);
-        LoadControl loadControl = new DefaultLoadControl();
-        player = ExoPlayerFactory.newSimpleInstance(getActivity(), trackSelector, loadControl);
-        simpleExoPlayerView.setUseController(true);
-        simpleExoPlayerView.requestFocus();
-        simpleExoPlayerView.setPlayer(player);
+        TrackSelector selector = new DefaultTrackSelector(new AdaptiveTrackSelection.Factory(bandwidthMeter));
+        player = ExoPlayerFactory.newSimpleInstance(getActivity(), selector);
+        Handler handler =new Handler();
         if (!steps.getThumbnailURL().isEmpty()) {
             videoUri = Uri.parse(steps.getThumbnailURL());
         } else if (!steps.getVideoURL().isEmpty()) {
@@ -87,103 +69,97 @@ public class StepDetial extends Fragment implements VideoRendererEventListener {
             simpleExoPlayerView.setVisibility(View.GONE);
         }
         if (videoUri != null) {
-            DefaultBandwidthMeter bandwidthMeterA = new DefaultBandwidthMeter();
-            DefaultDataSourceFactory dataSourceFactory =
-                    new DefaultDataSourceFactory(getActivity(), Util.getUserAgent(getActivity(), "bakingapp")
-                            ,bandwidthMeterA);
-            ExtractorsFactory extractorsFactory = new DefaultExtractorsFactory();
-            MediaSource videoSource =
-                    new HlsMediaSource(videoUri, dataSourceFactory, 1, null, null);
-            final LoopingMediaSource loopingSource = new LoopingMediaSource(videoSource);
-            player.prepare(loopingSource);
-            player.addListener(new ExoPlayer.EventListener() {
-                @Override
-                public void onTimelineChanged(Timeline timeline, Object manifest) {
-
-                }
-
-                @Override
-                public void onTracksChanged(TrackGroupArray trackGroups, TrackSelectionArray trackSelections) {
-
-                }
-
-                @Override
-                public void onLoadingChanged(boolean isLoading) {
-
-                }
-
-                @Override
-                public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
-
-                }
-
-                @Override
-                public void onRepeatModeChanged(int repeatMode) {
-
-                }
-
-                @Override
-                public void onPlayerError(ExoPlaybackException error) {
-                    player.stop();
-                    player.prepare(loopingSource);
-                    player.setPlayWhenReady(true);
-                }
-
-                @Override
-                public void onPositionDiscontinuity() {
-
-                }
-
-                @Override
-                public void onPlaybackParametersChanged(PlaybackParameters playbackParameters) {
-
-                }
-            });
+            DefaultHttpDataSourceFactory sourceFactory = new DefaultHttpDataSourceFactory(userAgent);
+            MediaSource source = new ExtractorMediaSource
+                    (videoUri, sourceFactory, new DefaultExtractorsFactory(), handler, null);
+            simpleExoPlayerView.setPlayer(player);
+            simpleExoPlayerView.setUseController(true);
+            simpleExoPlayerView.requestFocus();
+            player.seekTo(0);
+            player.prepare(source);
             player.setPlayWhenReady(true);
-            player.setVideoDebugListener(this);
         }
-
     }
 
-    @Override
-    public void onVideoEnabled(DecoderCounters counters) {
 
-    }
-
-    @Override
-    public void onVideoDecoderInitialized(String decoderName, long initializedTimestampMs, long initializationDurationMs) {
-
-    }
-
-    @Override
-    public void onVideoInputFormatChanged(Format format) {
-
-    }
-
-    @Override
-    public void onDroppedFrames(int count, long elapsedMs) {
-
-    }
-
-    @Override
-    public void onVideoSizeChanged(int width, int height, int unappliedRotationDegrees, float pixelWidthHeightRatio) {
-
-    }
-
-    @Override
-    public void onRenderedFirstFrame(Surface surface) {
-
-    }
-
-    @Override
-    public void onVideoDisabled(DecoderCounters counters) {
-
-    }
+//    private void showData() {
+//        BandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
+//        TrackSelection.Factory videoTrackSelectionFactory = new AdaptiveTrackSelection.Factory(bandwidthMeter);
+//        TrackSelector trackSelector = new DefaultTrackSelector(videoTrackSelectionFactory);
+//        LoadControl loadControl = new DefaultLoadControl();
+//        player = ExoPlayerFactory.newSimpleInstance(getActivity(), trackSelector, loadControl);
+//        simpleExoPlayerView.setUseController(true);
+//        simpleExoPlayerView.requestFocus();
+//        simpleExoPlayerView.setPlayer(player);
+//        if (!steps.getThumbnailURL().isEmpty()) {
+//            videoUri = Uri.parse(steps.getThumbnailURL());
+//        } else if (!steps.getVideoURL().isEmpty()) {
+//            videoUri = Uri.parse(steps.getVideoURL());
+//        } else {
+//            simpleExoPlayerView.setVisibility(View.GONE);
+//        }
+//        if (videoUri != null) {
+//            DefaultBandwidthMeter bandwidthMeterA = new DefaultBandwidthMeter();
+//            DefaultDataSourceFactory dataSourceFactory =
+//                    new DefaultDataSourceFactory(getActivity(), Util.getUserAgent(getActivity(), "bakingapp")
+//                            , bandwidthMeterA);
+//            ExtractorsFactory extractorsFactory = new DefaultExtractorsFactory();
+//            MediaSource videoSource =
+//                    new HlsMediaSource(videoUri, dataSourceFactory, 1, null, null);
+//            final LoopingMediaSource loopingSource = new LoopingMediaSource(videoSource);
+//            player.prepare(loopingSource);
+//            player.addListener(new ExoPlayer.EventListener() {
+//                @Override
+//                public void onTimelineChanged(Timeline timeline, Object manifest) {
+//
+//                }
+//
+//                @Override
+//                public void onTracksChanged(TrackGroupArray trackGroups, TrackSelectionArray trackSelections) {
+//
+//                }
+//
+//                @Override
+//                public void onLoadingChanged(boolean isLoading) {
+//
+//                }
+//
+//                @Override
+//                public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
+//
+//                }
+//
+//                @Override
+//                public void onRepeatModeChanged(int repeatMode) {
+//
+//                }
+//
+//                @Override
+//                public void onPlayerError(ExoPlaybackException error) {
+//                    player.stop();
+//                    player.prepare(loopingSource);
+//                    player.setPlayWhenReady(true);
+//                }
+//
+//                @Override
+//                public void onPositionDiscontinuity() {
+//
+//                }
+//
+//                @Override
+//                public void onPlaybackParametersChanged(PlaybackParameters playbackParameters) {
+//
+//                }
+//            });
+//            player.setPlayWhenReady(true);
+//            // player.setVideoDebugListener(this);
+//        }
+//
+//    }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         player.release();
     }
-
 }
